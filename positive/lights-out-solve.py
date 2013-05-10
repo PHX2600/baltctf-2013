7d32cdaa7cd0c0d5b38a386947641cc0ff0f7eb2
altf4 <altf4@phx2600.org>
2013-05-10 12:49:12 -0700

    + ‘positive/’ + ‘positive/lights-out-solve.py’ - ‘Untitled Folder/’

---
+++ positive/lights-out-solve.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+
+import sys
+import socket
+
+
+"""
+Automatic solver for Lights Out puzzles.
+Currently only square grids are supported.
+
+Includes a Gauss-Jordan elimination method for matrices
+defined on arbitrary fields. In particular, the function
+GF2inv inverts a matrix defined over the Galois Field GF(2) and
+determines its null-space.
+"""
+
+from operator import add
+from itertools import chain, combinations
+
+import numpy as np
+from scipy import ndimage
+
+class GF2(object):
+    """Galois field GF(2)."""
+   
+    def __init__(self, a=0):
+        self.value = int(a) & 1
+   
+    def __add__(self, rhs):
+        return GF2(self.value + GF2(rhs).value)
+   
+    def __mul__(self, rhs):
+        return GF2(self.value * GF2(rhs).value)
+   
+    def __sub__(self, rhs):
+        return GF2(self.value - GF2(rhs).value)
+   
+    def __div__(self, rhs):
+        return GF2(self.value / GF2(rhs).value)
+   
+    def __repr__(self):
+        return str(self.value)
+   
+    def __eq__(self, rhs):
+        if isinstance(rhs, GF2):
+            return self.value == rhs.value
+        return self.value == rhs
+   
+    def __le__(self, rhs):
+        if isinstance(rhs, GF2):
+            return self.value <= rhs.value
+        return self.value <= rhs
+   
+    def __lt__(self, rhs):
+        if isinstance(rhs, GF2):
+            return self.value <= rhs.value
+        return self.value < rhs
+   
+    def __int__(self):
+        return self.value
+   
+    def __long__(self):
+        return self.value
+   
+
+GF2array = np.vectorize(GF2)
+
+def gjel(A):
+    """Gauss-Jordan elimination."""
+    nulldim = 0
+    for i in xrange(len(A)):
+        pivot = A[i:,i].argmax() + i
+        if A[pivot,i] == 0:
+            nulldim = len(A) - i
+            break
+        row = A[pivot] / A[pivot,i]
+        A[pivot] = A[i]
+        A[i] = row
+       
+        for j in xrange(len(A)):
+            if j == i:
+                continue
+            A[j] -= row*A[j,i]
+    return A, nulldim
+
+def GF2inv(A):
+    """Inversion and eigenvectors of the null-space of a GF2 matrix."""
+    n = len(A)
+    assert n == A.shape[1], "Matrix must be square"
+   
+    A = np.hstack([A, np.eye(n)])
+    B, nulldim = gjel(GF2array(A))
+   
+    inverse = np.int_(B[-n:, -n:])
+    E = B[:n, :n]
+    null_vectors = []
+    if nulldim > 0:
+        null_vectors = E[:, -nulldim:]
+        null_vectors[-nulldim:, :] = GF2array(np.eye(nulldim))
+        null_vectors = np.int_(null_vectors.T)
+   
+    return inverse, null_vectors
+
+def lightsoutbase(n):
+    """Base of the LightsOut problem of size (n,n)"""
+    a = np.eye(n*n)
+    a = np.reshape(a, (n*n,n,n))
+    a = np.array(map(ndimage.binary_dilation, a))
+    return np.reshape(a, (n*n, n*n))
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+class LightsOut(object):
+    """Lights-Out solver."""
+   
+    def __init__(self, size=5):
+        self.n = size
+        self.base = lightsoutbase(self.n)
+        self.invbase, self.null_vectors = GF2inv(self.base)
+   
+    def solve(self, b):
+        b = np.asarray(b)
+        assert b.shape[0] == b.shape[1] == self.n, "incompatible shape"
+       
+        if not self.issolvable(b):
+            raise ValueError, "The given setup is not solvable"
+       
+        # Find the base solution.
+        first = np.dot(self.invbase, b.ravel()) & 1
+       
+        # Given a solution, we can find more valid solutions
+        # adding any combination of the null vectors.
+        # Find the solution with the minimum number of 1's.
+        solutions = [(first + reduce(add, nvs, 0))&1 for nvs in powerset(self.null_vectors)]
+        final = min(solutions, key=lambda x: x.sum())
+        return np.reshape(final, (self.n,self.n))
+   
+    def issolvable(self, b):
+        """Determine if the given configuration is solvable.
+A configuration is solvable if it is orthogonal to
+the null vectors of the base.
+"""
+        b = np.asarray(b)
+        assert b.shape[0] == b.shape[1] == self.n, "incompatible shape"
+        b = b.ravel()
+        p = map(lambda x: np.dot(x,b)&1, self.null_vectors)
+        return not any(p)
+   
+#assumes size 10
+def ConvertToArray(gridString):
+    array = np.array([[0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0]])
+    row = 0
+    column = 0
+    for c in gridString:
+        if c == '-':
+            array[row][column] = 1
+        column += 1
+        column %= 10
+        if(column == 0):
+            row += 1
+    return array
+
+def SendGrid(grid, socket):
+    for i in range(0,10):
+        for j in range(0,10):
+            if grid[i,j] == 1:
+                socket.send(str(i) + str(j))
+                print socket.recv(1000)
+
+def main():
+    """Example."""
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("194.106.195.60", 9502))
+
+    #header
+    s.recv(2000)
+
+    while True:
+        gridString = s.recv(2000)
+        gridString = gridString.replace('\n', '')
+
+        lo = LightsOut(10)
+
+        grid = ConvertToArray(gridString)
+        print "Converted to:"
+        print grid
+
+        bsol = lo.solve(grid)
+        print "Solution: "
+        print bsol
+
+        SendGrid(bsol, s)
+       # print s.recv(2000)
+
+if __name__ == '__main__':
+    main()
+
